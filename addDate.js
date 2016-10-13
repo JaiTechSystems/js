14b81ec8c8cf2ccf54ff4cdcfb0319d7a2ac9767
JaiTechSystems <mail.jaitechsytems@gmail.com>
2016-10-13 14:09:20 +0530

    Add files via upload

---
+++ addDate.js
@@ -0,0 +1,16 @@
+
+    //Setting value of date to today's date
+      $(document).ready( function() {
+        //Define today
+        var now = new Date();
+        var month = (now.getMonth() + 1);               
+        var day = now.getDate();
+        if(month < 10) 
+            month = "0" + month;
+        if(day < 10) 
+            day = "0" + day;
+        var today = now.getFullYear() + '-' + month + '-' + day;
+        //Add id_names
+        $('#datefield-4155-inputEl').val(today);
+        $('#datefield-4155-inputEl-2').val(today);
+    });
