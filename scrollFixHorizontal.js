18005c879af862bb3709d973de7e90406032842d
JaiTechSystems <mail.jaitechsytems@gmail.com>
2016-10-28 13:31:47 +0530

    Update scrollFixHorizontal.js

--- scrollFixHorizontal.js
+++ scrollFixHorizontal.js
@@ -48,9 +48,9 @@
       document.getElementsByTagName('table')[0].style.overflow="scroll";
 	    
       
-      document.getElementsByClassName('container2')[0].style.width=h+"px";
-      document.getElementsByClassName('container2')[0].style.minWidth=h+"px";
-      document.getElementsByClassName('container2')[0].style.overflow="scroll";
+      document.getElementsByClassName('containermain')[0].style.width=h+"px";
+      document.getElementsByClassName('containermain')[0].style.minWidth=h+"px";
+      document.getElementsByClassName('containermain')[0].style.overflow="scroll";
      
       
       document.getElementsByTagName('table')[0].style.margin="0px";
